92e6957cf5122319695366c8e604628b2ad1353c
guoconde <guoconde@gmail.com>
2022-04-23 15:32:59 -0300

    Feat: statirng project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <h1>Estou aqui</h1>
+    </BrowserRouter>
+  );
+}
